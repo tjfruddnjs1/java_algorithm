f101ecba37b58002c7ccb1ab335b4a1fa997c382
kyungwon seol <41010744+tjfruddnjs1@users.noreply.github.com>
2021-12-23 13:23:04 +0900

    Day 3. 백준 String 1문제 (10808)

---
+++ src/com/algorithm/String/Alphabet_10808.java
@@ -0,0 +1,23 @@
+package com.algorithm.String;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class Alphabet_10808 {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    String input = scanner.nextLine();
+
+    int[] alphabet_number = new int[26];
+    char[] alphabet = input.toCharArray();
+
+    for(char alpha : alphabet){
+      alphabet_number[Character.getNumericValue(alpha)-10]++;
+    }
+
+    IntStream stream = Arrays.stream(alphabet_number);
+    stream.forEach(v -> System.out.print(v+" "));
+  }
+}
