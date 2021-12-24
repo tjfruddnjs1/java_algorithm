ef85e02c347ebf968ac17519a357faf18fbd4933
kyungwon seol <41010744+tjfruddnjs1@users.noreply.github.com>
2021-12-24 20:04:16 +0900

    Day 4. SolutionFrame (BFS,DFS) 추가

---
+++ src/com/algorithm/solutionFrame.java
@@ -0,0 +1,26 @@
+package com.algorithm;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class solutionFrame {
+  static int input;
+
+  public static void function(){
+
+  }
+
+  public static void main(String[] args) throws IOException {
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringTokenizer st = new StringTokenizer(reader.readLine());
+
+    writer.flush();
+    writer.close();
+    reader.close();
+  }
+}
