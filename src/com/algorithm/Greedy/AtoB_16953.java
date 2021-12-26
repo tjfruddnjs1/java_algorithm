f2c1a265525c601d2d1fd2330877fa75c4d87d77
kyungwon seol <41010744+tjfruddnjs1@users.noreply.github.com>
2021-12-26 21:26:10 +0900

    Day 6. 백준 Greedy & BFS (1026, 1697, 16953)

---
+++ src/com/algorithm/Greedy/AtoB_16953.java
@@ -0,0 +1,46 @@
+package com.algorithm.Greedy;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class AtoB_16953 {
+    static long inputX;
+    static long purposeX;
+    static int answer;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(reader.readLine());
+        inputX = Long.parseLong(st.nextToken());
+        purposeX = Long.parseLong(st.nextToken());
+        answer = 1;
+
+        while(purposeX != inputX){
+            if(purposeX < inputX) {
+                answer = -1;
+                break;
+            }
+
+            int unit = (int)(purposeX % 10);
+
+            if(unit != 1 && purposeX % 2 != 0){
+                answer = -1;
+                break;
+            }
+
+            if(purposeX % 2 == 0) purposeX /= 2;
+            else purposeX /= 10;
+            answer++;
+        }
+
+        writer.write(String.valueOf(answer));
+        writer.flush();
+        writer.close();
+        reader.close();
+    }
+}
---
+++ src/com/algorithm/Search/HideAndSeek_1697.java
@@ -0,0 +1,64 @@
+package com.algorithm.Search;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class HideAndSeek_1697 {
+    static int[] map;
+    static boolean[] visited;
+    static int inputX;
+    static int purposeX;
+    static int maxSize;
+    static int[] direction_x = {-1, 1};
+
+    public static int bfs(int x){
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(x);
+        map[x] = 1;
+        visited[x] = true;
+
+        while(!queue.isEmpty()){
+            int nowX = queue.poll();
+            if(nowX == purposeX) break;
+
+            for(int i=0;i<3;i++){
+                int nextX;
+                if(i == 2) nextX = nowX * 2;
+                else nextX = nowX + direction_x[i];
+
+                if(nextX < 0 || nextX >= maxSize) continue;
+                if(visited[nextX]) continue;
+
+                queue.offer(nextX);
+                map[nextX] += map[nowX] + 1;
+                visited[nextX] = true;
+            }
+        }
+
+        return map[purposeX] - 1;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(reader.readLine());
+        inputX = Integer.parseInt(st.nextToken());
+        purposeX = Integer.parseInt(st.nextToken());
+
+        maxSize = Math.max((inputX*2)+1 ,(purposeX*2) +1);
+
+        map = new int[maxSize];
+        visited = new boolean[maxSize];
+
+        writer.write(String.valueOf(bfs(inputX)));
+        writer.flush();
+        writer.close();
+        reader.close();
+    }
+}
