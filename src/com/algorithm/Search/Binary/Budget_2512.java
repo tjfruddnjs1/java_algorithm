142f778aa78f68ccad6ceb32980b274ce7f17ec2
kyungwon seol <41010744+tjfruddnjs1@users.noreply.github.com>
2022-01-03 13:29:01 +0900

    Day 10. 백준 Binary Search 3문제 (1072, 2343, 2512)

---
+++ src/com/algorithm/Search/Binary/Budget_2512.java
@@ -0,0 +1,54 @@
+package com.algorithm.Search.Binary;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Budget_2512 {
+    static int[] budgets;
+    static long whole_budget;
+    static long answer;
+
+    public static void binarySearch(long low, long high){
+        while(low <= high){
+            long mid = (low + high) / 2;
+            long sum = 0;
+            for(int budget : budgets){
+                if(budget > mid) sum += mid;
+                else sum += budget;
+            }
+
+            if(sum > whole_budget) high = mid - 1;
+            else {
+                low = mid + 1;
+                answer = Math.max(answer, mid);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int input = Integer.parseInt(reader.readLine());
+        budgets = new int[input];
+
+        StringTokenizer st = new StringTokenizer(reader.readLine());
+        long low = 0;
+        long high = 0;
+        for(int i=0;i<input;i++){
+            budgets[i] = Integer.parseInt(st.nextToken());
+            high = Math.max(budgets[i], high);
+        }
+
+        whole_budget= Long.parseLong(reader.readLine());
+        binarySearch(low, high);
+        writer.write(String.valueOf(answer));
+        writer.flush();
+        writer.close();
+        reader.close();
+    }
+}
---
+++ src/com/algorithm/Search/Binary/GuitarLesson_2343.java
@@ -0,0 +1,63 @@
+package com.algorithm.Search.Binary;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class GuitarLesson_2343 {
+
+    static int[] lessons;
+    static int parse;
+
+    public static int binarySearch(int low, int high) {
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int sum = 0;
+            int cnt = 0;
+
+            for (int lesson : lessons) {
+                if (lesson + sum > mid) {
+                    sum = 0;
+                    cnt++;
+                }
+                sum += lesson;
+            }
+            if(sum != 0) cnt++;
+
+            if (cnt > parse) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return low;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(reader.readLine());
+
+        int input = Integer.parseInt(st.nextToken());
+        parse = Integer.parseInt(st.nextToken());
+        lessons = new int[input];
+
+        st = new StringTokenizer(reader.readLine());
+        int low = 0;
+        for (int i = 0; i < input; i++) {
+            lessons[i] = Integer.parseInt(st.nextToken());
+            low = Math.max(low, lessons[i]);
+        }
+
+        int high = Arrays.stream(lessons).sum();
+        writer.write(String.valueOf(binarySearch(low, high)));
+        writer.flush();
+        writer.close();
+        reader.close();
+    }
+
+}
